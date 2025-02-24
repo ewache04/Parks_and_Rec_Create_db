1c254c01a5feb58ebcbfb27dd6ca9e2f5933b759
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2025-02-24 06:21:50 -0500

    Update solutions_to_order_request_1.sql

--- employee_demography/solutions_to_order_request_1.sql
+++ employee_demography/solutions_to_order_request_1.sql
@@ -1,25 +1,25 @@
 -- Data Analist: Jeremiah
 -- This solution is for "order_request_1" by "John Doe"
 
--- #1. A complete list of all employee by demographic data
+-- #1. Complete list of all employee demographic data
 SELECT * 
 FROM parks_and_recreation.employee_demographics;
 
--- #2. Specific columns, including employee ID, first name, last name, age, gender, and birth date
+-- #2. Specific columns (employee ID, first name, last name, age, gender, birth date)
 SELECT employee_ID, first_name, last_name, age, gender, birth_date
 FROM parks_departments.employee_demographics;
 
--- #3. A filtered list of employees who are older than 30.
+-- #3. Employees older than 30
 SELECT * 
 FROM parks_and_recreation.employee_demographics 
 WHERE age > '30';
 
--- #4. A dataset that includes only male employees.
+-- #4. Only male employees
 SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE gender = 'male';
 
--- #5. A list of employees who were born after 1990.
+-- #5. Employees born after 1990
 SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE age > "1990-01-01";
@@ -71,7 +71,7 @@ SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE LOWER(SUBSTRING(first_name, 1,1)) = LOWER(SUBSTRING(first_name, -1,1));
 
--- #15 The gender distribution percentage of all employees.
+-- #15 Employees whose first name starts and ends with the same letter
 SELECT gender,
 COUNT(*) AS fre_gender,
 ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) 
@@ -79,14 +79,14 @@ FROM parks_and_recreation.employee_demographics), 2) AS gender_percentage
 FROM parks_and_recreation.employee_demographics
 GROUP BY gender;
 
--- #16 The top 5 most common last names among employees.
+-- #16 Gender distribution percentage
 SELECT last_name, COUNT(*) AS freq_last_name
 FROM parks_and_recreation.employee_demographics
 GROUP BY last_name 
 ORDER BY freq_last_name DESC
 LIMIT 5;
 
--- #16 A list of employees who are the only ones with their last name in the company.
+-- #16 Top 5 most common last names
 SELECT ed.*
 FROM parks_and_recreation.employee_demographics ed
 JOIN (
@@ -97,7 +97,7 @@ JOIN (
 ) unique_last_names
 ON ed.last_name = unique_last_names.last_name; 
 
--- #17 A list of employees who are the only ones with their last name in the company.
+-- #17  Employees who are the only ones with their last name
 SELECT ed. *
 FROM parks_and_recreation.employee_demographics ed
 join (
@@ -107,4 +107,20 @@ join (
     HAVING COUNT(*) = 1
 ) unique_last_names
 ON parks_and_recreation.employee_demographics.last_name = unique_last_names.last_name;
+
+-- #18  A count of employees grouped by birth year.
+SELECT  YEAR(birth_date) AS birth_year,  COUNT(*) AS employee_count 
+FROM parks_and_recreation.employee_demographics
+GROUP BY YEAR(birth_date)
+ORDER BY YEAR(birth_date);
+
+-- #18  A list of employees whose names contain at least three vowels.
+SELECT employee_id, first_name, last_name
+FROM parks_and_recreation.employee_demographics 
+WHERE 
+    (LENGTH(first_name) - LENGTH(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER(first_name), 'a', ''), 'e', ''), 'i', ''), 'o', ''), 'u', ''))) >= 3
+    OR 
+    (LENGTH(last_name) - LENGTH(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER(last_name), 'a', ''), 'e', ''), 'i', ''), 'o', ''), 'u', ''))) >= 3;
+
+
 
