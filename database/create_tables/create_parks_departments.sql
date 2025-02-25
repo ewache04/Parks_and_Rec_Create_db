2a050e48249c567d1b1c12645d4187d6ef0f8c00
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2025-02-25 08:35:27 -0500

    Create create_parks_departments.sql

---
+++ database/create_tables/create_parks_departments.sql
@@ -0,0 +1,6 @@
+-- Create parks_departments table
+CREATE TABLE parks_departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name varchar(50) NOT NULL,
+  PRIMARY KEY (department_id)
+);
