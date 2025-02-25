cbbd844247138af5bda80f3664cb89141137e3f2
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2025-02-25 08:35:30 -0500

    Create create_employee_salary.sql

---
+++ database/create_tables/create_employee_salary.sql
@@ -0,0 +1,9 @@
+-- Create employee_salary table
+CREATE TABLE employee_salary (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  occupation VARCHAR(50),
+  salary INT,
+  dept_id INT
+);
