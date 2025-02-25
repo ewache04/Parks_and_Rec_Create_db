816e0836d722f44842aed2cfd5363d0e7449b090
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2025-02-25 08:35:33 -0500

    Create create_employee_demographics.sql

---
+++ database/create_tables/create_employee_demographics.sql
@@ -0,0 +1,10 @@
+-- Create employee_demographics table
+CREATE TABLE employee_demographics (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  age INT,
+  gender VARCHAR(10),
+  birth_date DATE,
+  PRIMARY KEY (employee_id)
+);
