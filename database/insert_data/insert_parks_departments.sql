e13b0d0aa79fbe04c7904b022a2499b63f6be0fb
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2025-02-25 08:35:19 -0500

    Create insert_parks_departments.sql

---
+++ database/insert_data/insert_parks_departments.sql
@@ -0,0 +1,9 @@
+-- Insert data into parks_departments
+INSERT INTO parks_departments (department_name)
+VALUES
+('Parks and Recreation'),
+('Animal Control'),
+('Public Works'),
+('Healthcare'),
+('Library'),
+('Finance');
